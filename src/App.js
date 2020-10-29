a73abc53f53444e6e4424963960514efe8a7f8e0
Kyle Masterson <kylemasterson25@gmail.com>
2020-10-28 20:09:41 -0400

    fade and other effects

--- src/App.js
+++ src/App.js
@@ -7,34 +7,43 @@ import { Projects } from './Projects';
 import melody from './assets/melody.jpg';
 import rentie from './assets/rentie.jpg';
 import profile from './assets/profile.jpg';
+import Fade from 'react-reveal/Fade';
 
 
 function App() {
   return (
     <div className="App">
         <div id="hero" className="jumbotron">
-      <div className="container">
-        <h1 className="hero-title load-hidden">
-          Hi, i'm <span className="text-color-main">Kyle</span>.
-          <br />
-          The Developer you <span className="text-color-main">need</span>.
-        </h1>
-        <p className="hero-cta load-hidden">
-        <a href='#about'><Button>Discover More</Button></a>
-        </p>
-      </div>
-    </div>
-        <div id='about' className='tc'>
-          <h1 className='tc white ma0'>About Me</h1>
-          <div className='about-me'>
-            <img src={profile} alt="Profile" />
-            <p>jjrebjjcmwcejw  nre nrbnbn ewn nr r r  rbrbrbvv r vrbr  ver ve  ve r  vr  r r r e er  v  e r er r  re vr wv v  vd  dv </p>
+          <Fade right>
+          <div className="container">
+            <h1 className="hero-title load-hidden">
+              Hi, i'm <span className="text-color-main">Kyle</span>.
+              <br />
+              The Developer you <span className="text-color-main">need</span>.
+            </h1>
+            <a href='#about'><Button>Discover More</Button></a>
           </div>
+          </Fade>
         </div>
-          <div className='tc pa3' id='cards-div'>
-            <Card name={Projects[0].name} description={Projects[0].description} image={melody} />
-            <Card name={Projects[1].name} description={Projects[1].description} image={rentie} />
+        <section id='about' className='tc'>
+          <div className='about-padding'>
+            <Fade left>
+            <h1 className='tc white ma0'>About Me</h1>
+            <div className='about-me'>
+              <img src={profile} alt="Profile" />
+              <p className='white'>jjrebjjcmwcejw  nre nrbnbn ewn nr r r  rbrbrbvv r vrbr  ver ve  ve r  vr  r r r e er  v  e r er r  re vr wv v  vd  dv </p>
+            </div>
+            </Fade>
           </div>
+        </section>
+        <section className='tc pa3' id='cards-div'>
+          <Fade bottom>
+            <Card name={Projects[0].name} description={Projects[0].description} image={melody} />
+          </Fade>
+          <Fade bottom>
+            <Card name={Projects[1].name} description={Projects[1].description} image={rentie} />
+          </Fade>
+        </section>
           <Particles
             params={{
               particles: {
@@ -57,6 +66,7 @@ function App() {
           }}
           />
     </div>
+
   );
 }
 
--- src/Projects.js
+++ src/Projects.js
@@ -1,14 +1,10 @@
-import melody from './assets/melody.jpg';
-import rentie from './assets/melody.jpg';
 export const Projects = [
   {
     name: 'Melody',
-    description: 'Melody is an instrument rental app. Users are able to filter by instrument, create a new listing, see the geographic location of any listing, and book an instrument rental for a specified period of time. Melody is built using Ruby on Rails, Javascript, CSS, and HTML.',
-    image: {melody}
+    description: 'Melody is an instrument rental app. Users are able to filter by instrument, create a new listing, see the geographic location of any listing, and book an instrument rental for a specified period of time. Melody is built using Ruby on Rails, Javascript, CSS, and HTML.'
   },
   {
     name: 'Rentie',
-    description: 'Rentie is built using Ruby on Rails, Javascript, Html, and CSS. It allows property owners to manage their daily tasks and contact list, track their finances, and add or delete a building or unit from their profile. The tenant side of the app allows tenants to submit maintenance requests directly to the property owner and pay their rent online using Stripe.',
-    image: {rentie}
+    description: 'Rentie is built using Ruby on Rails, Javascript, Html, and CSS. It allows property owners to manage their daily tasks and contact list, track their finances, and add or delete a building or unit from their profile. The tenant side of the app allows tenants to submit maintenance requests directly to the property owner and pay their rent online using Stripe.'
   }
 ];
